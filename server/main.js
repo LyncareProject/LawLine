1b7420e52a64e671d73d361a94892c3ffb32fcce
Kun Woo Kim <kimkuns98@gmail.com>
2023-11-09 14:42:54 +0900

    Retry

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const app = require('./src/app');
+// const server = require('./websocket');
 
 const { PORT, WSPORT } = require('./src/common');
 
@@ -11,6 +12,9 @@ connectToDatabase()
         app.listen(PORT, () => {
             console.log(`Main Server is listening at PORT : ${PORT}`);
         }) 
+        // server.listen(WSPORT, () => {
+        //     consoe.log(`WebSocket lServer is listening at http://localhost:${ WSPORT }`);
+        // });
     })
     .catch(error => {
         console.error('Failed to connect to MongoDB:', error);
