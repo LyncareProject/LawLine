ec249804476cc311f73c3d4e8a9a53407626be33
Kun Woo Kim <kimkuns98@gmail.com>
2023-11-16 13:06:45 +0900

    client build

--- client/src/pages/CounselResult/CounselResult.jsx
+++ client/src/pages/CounselResult/CounselResult.jsx
@@ -1,9 +1,6 @@
-import { useEffect } from "react";
-import iconcall from "./../../components/images/iconcall.png";
 import "./CounselResult.css";
 import { useLocation } from "react-router";
 import Title from "../../components/Title/Title";
-import TextAreaWrap from "../../components/TextAreaWrap/TextAreaWrap";
 import Text from "../../components/Text/Text";
 import ImmutableTextWrap from "../../components/ImmutableTextWrap/ImmutableTextWrap";
 
@@ -13,14 +10,11 @@ const CounselResult = () => {
   return (
     <div className="Wrap">
       <Title title={"내 문의 조회하기"} margin={"50px 0"} />
-
-      <div className=" Phone_number">
-        <p>
-          <img src={iconcall} alt="전화기아이콘" />
-          전화번호
-        </p>
-        <input type="text" placeholder={state.phone} />
-      </div>
+      <ImmutableTextWrap
+        label={"전화번호"}
+        value={state.phone}
+        margin={"25px 0"}
+      />
       <ImmutableTextWrap
         label={"문의내용"}
         value={state.desc}
