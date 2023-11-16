6801d21815509c944e57f40c2d0758782ae93901
Kun Woo Kim <kimkuns98@gmail.com>
2023-11-16 13:15:38 +0900

    test server open

--- client/src/pages/CounselConfirm/CounselConfirm.jsx
+++ client/src/pages/CounselConfirm/CounselConfirm.jsx
@@ -3,29 +3,34 @@ import iconcall from "./../../components/images/iconcall.png";
 import iconkey from "./../../components/images/iconkey.png";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router";
+import Text from "../../components/Text/Text";
+import ImmutableTextWrap from "../../components/ImmutableTextWrap/ImmutableTextWrap";
+import Button from "../../components/Button/Button";
 
 const CounselConfirm = () => {
   const { state } = useLocation();
-  console.log(state)
+
   const navigate = useNavigate();
   return (
-    <div className="Completed Wrap">
-      <p className="Ball"></p>
-      <p className="Check_b">
-        <button
-          onClick={() => {
-            navigate("/counsel/search");
-          }}
-        >
-          조회하기
-        </button>
-      </p>
-
-      <div className="Inquiry_details">
-        <h4>문의내용</h4>
-        <textarea name="" id="" cols="30" rows="10" value={state}></textarea>
-      </div>
-      <h5>문의 접수가 완료되었습니다</h5>
+    <div className="Wrap">
+      <Text
+        text={"문의 접수가 완료되었습니다"}
+        textAlign={"center"}
+        fontSize={25}
+        fontWeight={700}
+        fontColor={"#00c126"}
+        margin={"25px"}
+      />
+      <ImmutableTextWrap label={"문의내용"} value={state} margin={"25px 0"} />
+      <Button
+        buttonName={"조회하기"}
+        buttonColor={"#00C126"}
+        buttonTextColor={"#FFF"}
+        buttonMargin={"10px 0"}
+        pressButton={() => {
+          navigate("/counsel/search");
+        }}
+      />
     </div>
   );
 };
